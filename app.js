cb46f4f31d38a069c4b7a9b35babac610fbbb7a3
Lizeth <mendezlizeth084@gmail.com>
2022-04-13 10:46:49 -0700

    Created matchingServingIdWithCount Function to Solve Problem 3 in App File

--- app.js
+++ app.js
@@ -164,6 +164,21 @@ console.log('Italian Cuisines', italianServingSize)
 //3. Create a function that will return only dishes whose serving id number matches their serving count.
 //Filter
 
+function matchingServingIdWithCount(){
+    let results= dishes.filter(function(el){
+        if (el.id == el.servings){
+            return true;
+        }
+        else{
+            return false;
+        }
+    })
+    return results    
+}
+
+let matchingDishesIDwithServing= matchingServingIdWithCount();
+console.log('Dishes whose Id matches Serving', matchingDishesIDwithServing)
+
 //4. Create a function that will return only dishes whose serving count is even.
 //Filter
 
